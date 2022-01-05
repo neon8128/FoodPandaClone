06a164276c42a6393b75fd2a89a6cfe878f22cba
David Suciu <github@neon8128.33mail.com>
2022-01-05 14:53:01 +0200

    changes

--- food-panda/src/Components/Home/HomeComponent.js
+++ food-panda/src/Components/Home/HomeComponent.js
@@ -8,36 +8,15 @@ const HomeComponent = () => {
   const [name, SetName] = useState("");
   const[restaurant,SetRestaurant] = useState('');
   const context = useContext(AuthContext);  
-  useEffect(async () => {
-    try{
-      let req = await axios("https://localhost:44321/token/get", {
-      method: "GET",
-      withCredentials: true,
-    });
-    const user = req.data;
+ 
    
-    SetName(user);
-    console.log(context.token);
-    let get = await axios("https://localhost:44321/restaurants/getall", {
-      method: "GET",
-      withCredentials: true,
-      headers: { Authorization: `Bearer ${context.token}` }
-    });
-    const restaurant = get.data;
-    SetRestaurant(restaurant);
-   
-    }
-    catch(e)
-    {
-      console.log(e);
-    }
+  
     
-  });
+
 
   return(
     <Grid container component="main" sx={{ height: "100vh" }}>
-    <div>{name? 'Hi '+name: "You are not logged in!"}</div>
-    <h1>{restaurant}</h1>
+    
     </Grid>
   );
 };
--- food-panda/src/Context/auth-context.js
+++ food-panda/src/Context/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AuthContext = React.createContext({
@@ -14,19 +14,39 @@ const AuthContext = React.createContext({
 export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(null);
 
-  const tokenHandler = async() =>{
+  useEffect = async() =>{
 
-   const response= await axios("https://localhost:44321/token/get", {
-      method: "GET",
-      withCredentials: true,
-    });
-              
+    await axios.get("https://localhost:44321/token/get",{ withCredentials: true })
     
-    setToken(response.data);
+        .then((response) => {
+          setToken(response.data.rawData);
+        })
+        .catch((error) => {
+        // Error 😨
+        if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+           
+        } else if (error.request) {
+
+          console.log(error.request);
+        } else {
+            // Something happened in setting up the request and triggered an Error
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+        return false;
+    });
+   
 
   }
  
-  const userIsLoggedIn = !!(token||tokenHandler());
+  const  userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
