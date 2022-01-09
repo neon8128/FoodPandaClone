645207a0782273cedd4ea18fa565203340ef1972
David Suciu <github@neon8128.33mail.com>
2022-01-09 16:50:56 +0200

    First Version of navbar

--- food-panda/src/Pages/RegisterPage.js
+++ food-panda/src/Pages/RegisterPage.js
@@ -1,7 +1,15 @@
+import RegisterForm from "../Components/Auth/RegisterForm";
+import { useContext } from "react";
+import AuthContext from "../Context/auth-context";
+import { Navigate } from "react-router-dom";
 
-import RegisterForm from '../Components/Auth/RegisterForm'
-
-const RegisterPage =() =>{
-    return <RegisterForm/>;
+const RegisterPage = () => {
+  let context = useContext(AuthContext);
+  let isAuthenticated = context.isLoggedIn;
+  if (!isAuthenticated) {
+    return <RegisterForm />;
+  } else {
+    return <Navigate to="/" />;
+  }
 };
 export default RegisterPage;
