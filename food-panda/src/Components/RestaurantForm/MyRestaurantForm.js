2f458a29df93c22f60b33d5e05010dd9a6a76f24
David Suciu <github@neon8128.33mail.com>
2022-01-18 19:00:12 +0200

    my restaurant page and bearer token

---
+++ food-panda/src/Components/RestaurantForm/MyRestaurantForm.js
@@ -0,0 +1,5 @@
+
+export const MyRestaurantForm = (props) =>{
+    return <div>{props.name}</div>
+}
+export default MyRestaurantForm;
---
+++ food-panda/src/Pages/MyRestaurantPage.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState,useContext } from "react";
+import { useAsync } from "react-use";
+import MyRestaurantForm from "../Components/RestaurantForm/MyRestaurantForm";
+import AuthContext from "../Context/auth-context";
+
+
+
+const MyRestaurantPage = () =>{
+  
+  const  context = useContext(AuthContext);
+  const token = context.token;
+  console.log(token);
+  const url =" https://localhost:44321/restaurants/getbyuser";
+  const [result, setResult] = useState(null);
+    const getRestaurant = async() =>{
+      await fetch(url,{
+        method: 'get',
+        headers : {
+          'Content-Type' : 'application/json',
+          'Accept' : 'application/json',
+          Authorization: `Bearer ${token}`,
+        }
+      })
+        
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setResult([jsonResponse.data][0]);
+     });
+    }
+    let state = useAsync(getRestaurant,[]);
+    console.log(result);
+    if(state.loading) {return <div>Loading Restaurant</div> }
+    return <MyRestaurantForm restaurant={result}/>
+  }
+  export default MyRestaurantPage;
